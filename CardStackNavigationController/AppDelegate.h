5b67f314852647f9e07af6c8f7e2e407e008faa6
William Chang <willdotchang@gmail.com>
2013-09-05 22:39:49 -0400

    Initial Commit

---
+++ CardStackNavigationController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CardStackNavigationController
+//
+//  Created by William Chang on 2013-09-05.
+//  Copyright (c) 2013 William Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
